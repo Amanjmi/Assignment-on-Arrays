c2cfec9263154c3fb71cf72ea6d73da79fef8159
Aman <amanurrehman68@gmail.com>
2022-11-13 09:35:58 +0530

    First code for assignment

---
+++ Array/src/Dupplicatevalueinarray.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class Dupplicatevalueinarray {
+			
+				public static void main(String[] args) {
+					int[] a = {10,18,12,21,22,21,12};
+					Arrays.sort(a);
+//					for(int ele:a) {
+//						System.out.print(ele +" ");
+//					}
+					for(int i=0;i<a.length;i++) {
+						for(int j=i+1;j<a.length-1;j++) {
+							if (a[i]==a[j]) {
+								System.out.println(a[i]+" is the duplicate value in the given array");
+								break;
+							}
+						}
+					}
+
+					}
+					}
